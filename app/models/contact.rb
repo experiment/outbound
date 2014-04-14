e79c4bc7057f8bcbe6b7245e06a736ddf33c0b24
Ryan Lower <rpjlower@gmail.com>
2014-04-14 13:10:09 -0700

    Use first name in first contact email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,4 +5,17 @@ class Contact < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
 
   has_many :emails
+
+  # Best guess of a contacts first name
+  def first_name
+    case name.split.size
+      when 1
+        name
+      when 2
+        name.split.first
+      else
+        name
+    end
+  end
+
 end
