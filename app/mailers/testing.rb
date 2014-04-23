2d331d1ea1922fab77fbee0714ce19ff9593047e
Ryan Lower <rpjlower@gmail.com>
2014-04-22 17:50:51 -0700

    Send test emails from oscarj

--- app/mailers/testing.rb
+++ app/mailers/testing.rb
@@ -1,7 +1,9 @@
 class Testing < ActionMailer::Base
+  default to: 'ryan@experiment.com',
+          from: 'oscarj@experiment.com'
+
   def test
-    mail to: 'ryan@experiment.com',
-         subject: 'outbound test',
+    mail subject: 'outbound test',
          body: 'hello world'
   end
 end
