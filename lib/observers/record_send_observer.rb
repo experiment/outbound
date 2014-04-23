c06ced6f521eff5f2419c706d63ccbf2a916d77c
Ryan Lower <rpjlower@gmail.com>
2014-04-22 17:45:49 -0700

    Don't try to record send if message doesn't have a _recipient_contact_id

--- lib/observers/record_send_observer.rb
+++ lib/observers/record_send_observer.rb
@@ -9,6 +9,8 @@ class RecordSendObserver
   private
 
     def self.record_send!(message)
+      return unless message._recipient_contact_id
+
       contact = Contact.find message._recipient_contact_id
 
       contact.emails.create do |email|
