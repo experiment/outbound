6a50d1dde81e78e9f025d7f3f94b95a146f7c121
Ryan Lower <rpjlower@gmail.com>
2014-04-22 17:48:58 -0700

    Create html versions of plain text emails when sent via mandrill

---
+++ lib/interceptors/mandrill_headers_interceptor.rb
@@ -0,0 +1,19 @@
+# Registered in config/initializers/email.rb
+
+class MandrillHeadersInterceptor
+
+  def self.previewing_email(mail)
+    add_mandrill_headers mail
+  end
+
+  def self.delivering_email(mail)
+    add_mandrill_headers mail
+  end
+
+  private
+
+    def self.add_mandrill_headers(mail)
+      # Create HTML versions of plain text emails
+      mail.header['X-MC-AutoHtml'] = true
+    end
+end
