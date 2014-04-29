41bec3974eba1006331a3ff8ddf4b9e5b0d49719
Ryan Lower <rpjlower@gmail.com>
2014-04-29 15:54:49 -0700

    Mark email as opened on open webhook

--- app/controllers/mandrill_hooks_controller.rb
+++ app/controllers/mandrill_hooks_controller.rb
@@ -5,5 +5,20 @@ class MandrillHooksController < ApplicationController
 
   def handle_open(payload)
     Rails.logger.info payload
+
+    if payload['msg'] && payload['msg']['tags'].include?('outbound')
+      record_open! payload['msg']['email'], Time.at(payload['ts'])
+    end
   end
+
+  private
+
+    def record_open!(email, timestamp)
+      puts "record_open! #{email} at #{timestamp}"
+      if (contact = Contact.find_by(email: email))
+        # TODO, presumes email opened was first_contact
+        email = contact.emails.where(method: 'first_contact').take
+        email.opened! timestamp
+      end
+    end
 end
--- spec/controllers/mandrill_hooks_controller_spec.rb
+++ spec/controllers/mandrill_hooks_controller_spec.rb
@@ -2,4 +2,31 @@ require 'spec_helper'
 
 describe MandrillHooksController do
 
+    let(:timestamp) { Time.now }
+    let(:payload) do
+      {
+        event: 'open',
+        msg: {
+          ts: 1.day.ago.to_i,
+          subject: 'Open webhook',
+          sender: 'oscarj@experiment.com',
+          tags: [ 'outbound' ]
+        },
+        ts: timestamp.to_i
+      }
+    end
+
+  describe 'handle_open' do
+    let(:contact) { Contact.create! source: 'manual', email: 'ryan@experiment.com'}
+
+    it 'marks email as opened' do
+      payload[:msg][:email] = contact.email
+      email = contact.emails.create! method: 'first_contact'
+
+      post :create, mandrill_events: [ payload ].to_json
+
+      email.reload
+      expect(email.opened.to_i).to eq timestamp.to_i
+    end
+  end
 end
