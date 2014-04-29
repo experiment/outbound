39dc3a3f9d4c8f05f0217c89ec2bfefb441cc967
Ryan Lower <rpjlower@gmail.com>
2014-04-29 16:42:55 -0700

    Always send phone call version for now

--- test/mailers/previews/introductions_preview.rb
+++ test/mailers/previews/introductions_preview.rb
@@ -1,5 +1,5 @@
 class IntroductionsPreview < ActionMailer::Preview
   def first_contact
-    Introductions.first_contact(Contact.last.id, 'phone_call')
+    Introductions.first_contact(Contact.last.id)
   end
 end
