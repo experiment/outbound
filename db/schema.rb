35f26b85f5945eb303b40a5b81b679a98891fb5c
Ryan Lower <rpjlower@gmail.com>
2014-04-29 15:51:03 -0700

    Add opened timestamp to emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419001432) do
+ActiveRecord::Schema.define(version: 20140429225009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140419001432) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "version"
+    t.datetime "opened"
   end
 
   add_index "emails", ["contact_id"], name: "index_emails_on_contact_id", using: :btree
