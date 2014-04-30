36df133af548a868953d501221e326e181c6a17b
Ryan Lower <rpjlower@gmail.com>
2014-04-30 12:05:18 -0700

    Add project_created_at timestamp to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429232736) do
+ActiveRecord::Schema.define(version: 20140430190454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140429232736) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.json     "info"
+    t.datetime "project_created_at"
   end
 
   create_table "emails", force: true do |t|
