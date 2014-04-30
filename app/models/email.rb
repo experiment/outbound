f282daf490622af06876aec283ffe704706d21b7
Ryan Lower <rpjlower@gmail.com>
2014-04-30 10:39:21 -0700

    Add opened scope to emails

--- app/models/email.rb
+++ app/models/email.rb
@@ -4,6 +4,8 @@ class Email < ActiveRecord::Base
   # created on given day
   scope :on_date, lambda { |date| where("DATE_TRUNC('day', created_at) = ?", date) }
 
+  scope :opened, -> { where('opened IS NOT NULL') }
+
   def opened!(at)
     update_attributes! opened: at
   end
