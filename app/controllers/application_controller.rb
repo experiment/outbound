ffda500959711dd3d83322c6fb6e99c439e6255e
Ryan Lower <rpjlower@gmail.com>
2014-04-30 10:37:28 -0700

    Show sent and open rates per batch on homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def home
+    @dates = Email.select("DISTINCT DATE_TRUNC('day', created_at) AS created_at").map(&:created_at)
     render layout: nil
   end
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,9 @@
 class Email < ActiveRecord::Base
   belongs_to :contact
 
+  # created on given day
+  scope :on_date, lambda { |date| where("DATE_TRUNC('day', created_at) = ?", date) }
+
   def opened!(at)
     update_attributes! opened: at
   end
