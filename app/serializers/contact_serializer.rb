bbb24524a8d1705e51e636f3e248d7ecccb044c1
Ryan Lower <rpjlower@gmail.com>
2014-05-08 16:57:39 -0700

    Return nil for email event timestamps if no emails have been sent

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -5,14 +5,20 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :contacted_at, :opened_at, :replied_at
 
   def contacted_at
+    return nil unless object.emails.any?
+
     object.emails.first.created_at
   end
 
   def opened_at
+    return nil unless object.emails.any?
+
     object.emails.first.opened
   end
 
   def replied_at
+    return nil unless object.emails.any?
+
     object.emails.first.replied_at
   end
 end
