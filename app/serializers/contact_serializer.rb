eac30fe04eeeebf1b4bbbc6bbeb847d0b9291fce
Ryan Lower <rpjlower@gmail.com>
2014-05-17 17:37:07 -0700

    Add dead & unsubscribed booleans to contact serialiser

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -4,9 +4,19 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :email, :start_page_viewed_at, :project_created_at
   attributes :contacted_at
 
+  attributes :dead, :unsubscribed
+
   def contacted_at
     return nil unless object.emails.any?
 
     object.emails.first.created_at
+  end
+
+  def dead
+    object.outbound_process.dead?
+  end
+
+  def unsubscribed
+    object.outbound_process.unsubscribed?
   end
 end
