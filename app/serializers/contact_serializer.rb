a242a4d11b96f7447864f06369df986751c50366
Ryan Lower <rpjlower@gmail.com>
2014-05-17 17:32:13 -0700

    Remove opened_at & replied_at from serialised contact

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,23 +2,11 @@ class ContactSerializer < ActiveModel::Serializer
   self.root = false
 
   attributes :id, :email, :start_page_viewed_at, :project_created_at
-  attributes :contacted_at, :opened_at, :replied_at
+  attributes :contacted_at
 
   def contacted_at
     return nil unless object.emails.any?
 
     object.emails.first.created_at
-  end
-
-  def opened_at
-    return nil unless object.emails.any?
-
-    object.emails.first.opened
-  end
-
-  def replied_at
-    return nil unless object.emails.any?
-
-    object.emails.first.replied_at
   end
 end
