6e2d9d87745823aa1c17242874c58356bdc71f8c
Ryan Lower <rpjlower@gmail.com>
2014-06-19 18:37:42 -0700

    Real template for mass follow up email

--- app/mailers/follow_ups.rb
+++ app/mailers/follow_ups.rb
@@ -9,7 +9,38 @@ class FollowUps < Mailer
   def mass_follow_up(contact_id)
     @contact = Contact.find contact_id
 
-    mail subject: "Re: #{original_subject @contact}"
+    @keyword = if @contact.info.has_key? :source
+      @contact.info[:type]
+    else
+      case @contact.info[:journal]
+        when 'BMC Neuroscience', 'BMC Psychology', 'Behavioral and Brain Functions', 'BMC Neurology'
+          'neuroscience & psychology'
+        when 'BMC Cancer'
+          'cancer'
+        when 'BMC Evolutionary Biology', 'BMC Plant Biology', 'PLOS Biology', 'PLOS Computational Biology', 'BMC Systems Biology'
+          'biology'
+        when 'BMC Pediatrics'
+          'pediatric'
+        when 'BMC Genomics'
+          'genetics'
+        when 'Journal of Nanobiotechnology'
+          'nanobiology'
+        when 'PLOS Neglected Tropical Diseases'
+          'neglected disease'
+        when 'BMC Immunology'
+          'health related'
+        else
+          raise "No keyword for this journal (#{@contact.info[:journal]})"
+      end
+    end
+
+    subject = if rand(2) == 1
+      "Re: #{original_subject @contact}"
+    else
+      'Early-stage research funding - revisiting experiment.com'
+    end
+
+    mail subject: subject
   end
 
   private
