6289978e933e8e801fb874ce91971c62a7640000
Ryan Lower <rpjlower@gmail.com>
2014-06-19 17:51:28 -0700

    Add preview for mass_follow_up email

--- test/mailers/previews/follow_ups_preview.rb
+++ test/mailers/previews/follow_ups_preview.rb
@@ -2,4 +2,8 @@ class FollowUpsPreview < ActionMailer::Preview
   def interested_first_follow_up
     FollowUps.interested_first_follow_up(Contact.last.id)
   end
+
+  def mass_follow_up
+    FollowUps.mass_follow_up(Contact.last.id)
+  end
 end
