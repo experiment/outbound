fa4b579d671414d387f1b8a553eab6b2fa0920a2
Ryan Lower <rpjlower@gmail.com>
2014-06-19 17:50:32 -0700

    Add method to return original subject line

--- app/mailers/follow_ups.rb
+++ app/mailers/follow_ups.rb
@@ -3,16 +3,26 @@ class FollowUps < Mailer
   def interested_first_follow_up(contact_id)
     @contact = Contact.find contact_id
 
-    # TODO, record original subject and use that
-    # so they don't drift apart
-    subject = "Re: I saw your paper in #{@contact.info[:journal]}"
-
-    mail subject: subject
+    mail subject: "Re: #{original_subject @contact}"
   end
 
   def mass_follow_up(contact_id)
     @contact = Contact.find contact_id
 
-    mail subject: "Re: I saw your paper in #{@contact.info[:journal]}"
+    mail subject: "Re: #{original_subject @contact}"
   end
+
+  private
+
+    def original_subject(contact)
+      # TODO, record original subject and use that
+      # so they don't drift apart
+      if contact.info.has_key? :journal
+        "I saw your paper in #{contact.info[:journal]}"
+      elsif contact.info.has_key? :source
+        "I saw your story in #{contact.info[:source]}"
+      else
+        raise 'Unknown original subject'
+      end
+    end
 end
