54bc521500c3ae3e03e8be22c1e2374f5c1624eb
Ryan Lower <rpjlower@gmail.com>
2014-06-25 08:36:01 -0700

    Stop logging mandrill payloads

--- app/controllers/mandrill_hooks_controller.rb
+++ app/controllers/mandrill_hooks_controller.rb
@@ -4,23 +4,20 @@ class MandrillHooksController < ApplicationController
   authenticate_with_mandrill_keys! ENV['MANDRILL_WEBHOOK_KEY']
 
   def handle_open(payload)
-    Rails.logger.info payload
-
     if payload['msg'] && payload['msg']['tags'].include?('outbound')
       record_open! payload['msg']['email'], Time.at(payload['ts'])
     end
   end
 
   def handle_inbound(payload)
-    Rails.logger.info payload
-
     record_reply! payload['msg'], Time.at(payload['ts'])
   end
 
   private
 
     def record_open!(email, timestamp)
-      puts "record_open! #{email} at #{timestamp}"
+      Rails.logger.info "record_open! #{email} at #{timestamp}"
+
       if (contact = Contact.by_email(email).take)
         # TODO, presumes email opened was first_contact
         email = contact.emails.where(method: 'first_contact').take
